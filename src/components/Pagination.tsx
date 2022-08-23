a5c4ca112ba70addcb6ccf7122b2a62f927de094
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-08-23 13:13:56 -0300

    up

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,66 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+export function Pagination(){
+    return (
+        <Stack
+            direction='row'
+            spacing='6'
+            mt='8'
+            justify='space-between'
+            align='center'
+
+        >
+          <Box>
+            <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+          </Box>  
+          <Stack direction='row' spacing='2'>
+            <Button 
+                size='sm'
+                fontSize='xs'
+                width='4'
+                colorScheme='pink'
+                disabled
+                _disabled= {{
+                    bgcolor: 'pink',
+                    cursor: 'default',
+                }}
+                >
+                    1
+                </Button>
+                <Button 
+                size='sm'
+                fontSize='xs'
+                width='4'
+                bg='gray.700'
+                _hover={{
+                    bg: 'gray.500'
+                }}
+                >
+                    2
+                </Button>
+                <Button 
+                size='sm'
+                fontSize='xs'
+                width='4'
+                bg='gray.700'
+                _hover={{
+                    bg: 'gray.300'
+                }}
+                >
+                    3
+                </Button>
+                <Button 
+                size='sm'
+                fontSize='xs'
+                width='4'
+                bg='gray.700'
+                _hover={{
+                    bg: 'gray.300'
+                }}
+                >
+                    4
+                </Button>
+          </Stack>  
+        </Stack>
+    )
+}
